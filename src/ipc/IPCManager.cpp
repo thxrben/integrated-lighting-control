e553f150e3bb3a82405bc0a6f7ea599a90bc1442
Gandalf1783 <46635221+Gandalf1783@users.noreply.github.com>
2024-03-31 18:17:39 +0200

    Moved files into directory, made changes to CMake

---
+++ src/ipc/IPCManager.cpp
@@ -0,0 +1,76 @@
+#include "ipc/IPCManager.hpp"
+
+
+IPCManager::IPCManager(std::shared_ptr<Log> logger) {
+    this->logger = logger;
+    printf("[IPCManager] Initiating IPC...\n");
+    std::string defaultPort = currentConfig.get<std::string>("ipc.defaultPort");
+};
+
+void IPCManager::start() {
+    
+
+    printf("[IPCManager] Creating context...\n");
+    this->context = std::make_shared<zmqpp::context>();
+
+    printf("[IPCManager] Setting socket-type...\n");
+    this->type = zmqpp::socket_type::router;
+    printf("[IPCManager] Creating socket...\n");
+    this->socket = std::make_shared<zmqpp::socket>(*(this->context.get()),this->type);
+
+    printf("[IPCManager] Binding...\n");
+    this->socket->bind("tcp://127.0.0.1:5555");
+
+    printf("[IPCManager] Done lol \\o/\n");
+    //TODO: Start Thread to listen
+    this->shouldThreadStop = false;
+    this->zmqppThread = std::thread(&IPCManager::receiveLoop, this);
+
+};
+
+void IPCManager::receiveLoop() {
+    while(!this->shouldThreadStop) {
+        
+        //Receive ZMQ messages here
+        zmqpp::message message;
+        
+        this->socket->receive(message, false);
+        std::string s1 = message.get(0);
+        std::string s2 = message.get(1);
+        
+        std::cout << s1 << " " << s2 << std::endl;
+        
+        //ATTENTION: S2 is a String Object and IS INDEED VALUE-COMPARED HERE!
+        if(s2 == "LOGIN") {
+            zmqpp::message response;
+            response.add(s1);
+            response.add("");
+            response.add("OK");
+            this->socket->send(response);
+        }
+
+    }
+}
+
+IPCManager::~IPCManager() {
+    printf("[IPCManager] Closing IPC...\n");
+    this->shouldThreadStop = true;
+
+    printf("Closing Socket: \n");
+    std::string endpoint = this->socket->get<std::string>(zmqpp::socket_option::last_endpoint);
+
+    std::cout << "Unbinding: " << endpoint << std::endl;
+
+    this->socket->unbind(endpoint);
+    printf("ERRNO: %s\n", zmq_strerror(zmq_errno()));
+
+    zmq_close(*this->socket.get());
+    printf("ERRNO: %s\n", zmq_strerror(zmq_errno()));    
+
+    printf("Destroy Context: \n");
+    
+    zmq_ctx_term(*(this->context.get()));
+    printf("ERRNO: %s\n", zmq_strerror(zmq_errno()));
+
+    this->zmqppThread.join();
+};
